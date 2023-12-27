c04403296b2d9acce0e607e2c264134b94db074a
Tsotne <cotnemexuri@gmail.com>
2023-12-27 14:00:54 +0400

    Add content on HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,36 @@
 <script setup>
+import Sidebar from "@/components/Sidebar.vue";
+import StepsWrapper from "@/components/StepsWrapper.vue";
+import {computed, ref} from "vue";
+
+const currentStep = ref(1)
+
+const stepsListing = [
+  {title: "Personal info", description: "Please provide your name, email address, and phone number."},
+  {title: "Select your plan", description: "You have the option of monthly or yearly billing."},
+  {title: "Pick add-ons", description: "Add-ons help enhance your gaming experience."},
+  {title: "Finishing up", description: "Double-check everything looks OK before confirming."},
+  {
+    title: "Thank you!",
+    description: "Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com."
+  }
+]
+
+const stepData = computed(() => {
+  return stepsListing[currentStep.value - 1]
+})
 </script>
 
 <template>
-<h1>home page</h1>
+<div class="w-full h-dvh bg-[#EFF5FF] flex items-center justify-center">
+  <main class="bg-white p-4 w-[940px] h-[600px] rounded-[14px] grid grid-cols-[274px_1fr]">
+    <Sidebar :currentStep="currentStep"/>
+    <div class="flex items-center justify-center">
+      <StepsWrapper :title="stepData.title"
+                    :description="stepData.description"
+                    v-model:currentStep="currentStep"/>
+    </div>
+  </main>
+</div>
 </template>
+
