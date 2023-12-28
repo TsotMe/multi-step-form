850fe9bbd96711ba87b39c775a85c7d11a8c087a
Tsotne <cotnemexuri@gmail.com>
2023-12-28 15:25:39 +0400

    Initial second step component

---
+++ src/components/steps/SecondStep.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {ref} from "vue";
+import CustomToggle from "@/components/ui/CustomToggle.vue";
+
+const isYearly = ref(false)
+</script>
+
+<template>
+<div class="second-step">
+  <CustomToggle v-model="isYearly"/>
+</div>
+</template>
