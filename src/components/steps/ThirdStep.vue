b0d6ad669c39ba530ec7f8cfec288a1f6429e07c
TsotMe <76700394+TsotMe@users.noreply.github.com>
2023-12-29 20:57:06 +0400

    Third step (#7)
    * Add "selectedPlan" in store to use it on third and fourth steps
    
    * Initial third step component
    
    * Add Third Step
    
    ---------
    
    Co-authored-by: Tsotne <cotnemexuri@gmail.com>

---
+++ src/components/steps/ThirdStep.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AddOn from "@/components/third-step/AddOn.vue";
+import {useThirdStep} from "@/stores/ThirdStep.js";
+
+const thirdStepStore = useThirdStep()
+</script>
+
+<template>
+<div class="h-full flex flex-col gap-4 mt-[35px] ">
+  <AddOn title="Online service" description="Access to multiplayer games" :price="1" v-model="thirdStepStore.onlineService"/>
+  <AddOn title="Larger storage" description="Extra 1TB of cloud save" :price="2" v-model="thirdStepStore.largerStorage"/>
+  <AddOn title="Customizable profile" description="Custom theme on your profile" :price="2"
+         v-model="thirdStepStore.customizableProfile"/>
+</div>
+</template>
