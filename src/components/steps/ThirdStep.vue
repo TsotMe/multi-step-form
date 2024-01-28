7a990312c8832cf477e69d1d78ce4bac2471fd31
Tsotne <cotnemexuri@gmail.com>
2024-01-28 20:25:35 +0400

    Add responsive styles on Third step

--- src/components/steps/ThirdStep.vue
+++ src/components/steps/ThirdStep.vue
@@ -6,7 +6,7 @@ const thirdStepStore = useThirdStep()
 </script>
 
 <template>
-<div class="h-full flex flex-col gap-4 mt-[35px] ">
+<div class="h-full flex flex-col gap-4 mt-[35px] tablet:mt-[22px] tablet:gap-[12px]">
   <AddOn title="Online service" description="Access to multiplayer games" :price="1" v-model="thirdStepStore.onlineService"/>
   <AddOn title="Larger storage" description="Extra 1TB of cloud save" :price="2" v-model="thirdStepStore.largerStorage"/>
   <AddOn title="Customizable profile" description="Custom theme on your profile" :price="2"
--- src/components/third-step/AddOn.vue
+++ src/components/third-step/AddOn.vue
@@ -26,20 +26,20 @@ const calculatedPrice = computed(() => {
 <div
   :class="`${modelValue ?
    'border-[#483EFF]' : ''} w-full h-[81px] rounded-lg border border-solid border-[#D6D9E6] transition-all duration-200
-  hover:border-[#483EFF] flex items-center justify-between px-6 cursor-pointer`"
+  hover:border-[#483EFF] flex items-center justify-between px-6 cursor-pointer tablet:px-4 tablet:h-[62px]`"
   @click="emit('update:modelValue', !modelValue)"
 >
-  <div class="flex items-center gap-6">
+  <div class="flex items-center gap-6 tablet:gap-4">
     <div>
       <img v-show="modelValue" src="@/assets/images/checkbox-checked.svg" alt="">
       <img v-show="!modelValue" src="@/assets/images/checkbox-unchecked.svg" alt="">
     </div>
-    <div class="flex flex-col gap-2">
-      <h4 class="text-base text-[#022959] font-ubuntu-medium">{{ title }}</h4>
-      <p class="text-sm text-[#9699AA]">{{ description }}</p>
+    <div class="flex flex-col gap-2 tablet:gap-[3px]">
+      <h4 class="text-base text-[#022959] font-ubuntu-medium tablet:text-sm tablet:leading-4">{{ title }}</h4>
+      <p class="text-sm text-[#9699AA] tablet:text-xs tablet:leading-5">{{ description }}</p>
     </div>
   </div>
-  <span class="text-[#483EFF] text-sm">
+  <span class="text-[#483EFF] text-sm tablet:text-xs tablet:leading-5">
     {{ calculatedPrice }}
   </span>
 </div>
