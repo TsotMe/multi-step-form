784b8e3ca7cb127a93b4fe0c53509065995fd74c
Tsotne <cotnemexuri@gmail.com>
2024-01-28 19:29:08 +0400

    Add responsive styles on CustomInput.vue

--- src/components/ui/CustomInput.vue
+++ src/components/ui/CustomInput.vue
@@ -38,12 +38,13 @@ const value = computed({
 
 <template>
 <div class="w-full h-fit">
-  <div class="flex items-center justify-between mb-2">
-    <label class="text-[#022959]">{{ labelText }}</label>
-    <span class="font-ubuntu-bold text-[#EE374A]">{{ errorText }}</span>
+  <div class="flex items-center justify-between mb-2 tablet:mb-[3px]">
+    <label class="text-[#022959] tablet:text-xs">{{ labelText }}</label>
+    <span class="font-ubuntu-bold text-[#EE374A] tablet:text-xs">{{ errorText }}</span>
   </div>
   <input
-    class="w-full h-12 rounded-lg border border-solid border-[#D6D9E6] text-[#022959] pl-2.5 font-ubuntu-medium text-base focus:border-[#483EFF] placeholder:text-[#9699AA]"
+    class="w-full h-12 rounded-lg border border-solid border-[#D6D9E6] text-[#022959] pl-2.5 font-ubuntu-medium text-base
+    focus:border-[#483EFF] placeholder:text-[#9699AA] tablet:h-10 tablet:rounded tablet:text-sm"
     :type="type"
     :placeholder="placeholderText"
     v-model="value">
