4d30ce4c2ef39db79d65678908ba17f1f31de58d
Tsotne <cotnemexuri@gmail.com>
2024-01-28 18:52:40 +0400

    Add "ActionBar" component

---
+++ src/components/ActionBar.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {computed} from "vue";
+
+const props = defineProps({
+  currentStep: {
+    type: Number,
+    default: null
+  }
+})
+
+const emit = defineEmits(['prev', 'next'])
+
+const nextButtonName = computed(() => {
+  return props.currentStep === 4 ? 'Confirm' : 'Next Step'
+})
+</script>
+
+<template>
+<div :class="`${currentStep === 1 ? '!justify-end' : ''} flex items-center justify-between`">
+  <button
+    class="text-[#9699AA] font-ubuntu-medium" v-show="currentStep
+    !== 1" @click="emit('prev')">
+    Go Back
+  </button>
+  <button
+    :class="`${currentStep === 4 ? '!bg-[#483EFF]' : 'bg-[#022959]'} text-white py-3 px-6 rounded-lg font-ubuntu-medium`"
+    @click="emit('next', currentStep + 1)">
+    {{ nextButtonName }}
+  </button>
+</div>
+</template>
+
+<style scoped>
+
+</style>
